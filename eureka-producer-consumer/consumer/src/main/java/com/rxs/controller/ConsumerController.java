22985e6ab5d7a577cf2a4098a49cec11f56dff31
junareny <ryjlearn@163.com>
2020-05-16 03:48:41 +0800

    增加eureka消费使用例子

---
+++ eureka-producer-consumer/consumer/src/main/java/com/rxs/controller/ConsumerController.java
@@ -0,0 +1,25 @@
+package com.rxs.controller;
+
+import com.rxs.remote.RemoteHello;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConsumerController {
+
+    @Autowired
+    RemoteHello remoteHello;
+
+    /*@RequestMapping("/hello")
+    public String hello(@RequestParam(required = true, name = "name", defaultValue = "rxs2") String name) {
+        return remoteHello.hello(name);
+    }*/
+    //第二种传参方式
+    @RequestMapping("/hello/{name}")
+    public String hello(@PathVariable(name = "name", required = true) String name) {
+        return remoteHello.hello(name);
+    }
+}
---
+++ eureka-producer-consumer/consumer/src/main/java/com/rxs/remote/RemoteHello.java
@@ -0,0 +1,11 @@
+package com.rxs.remote;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "producer")
+public interface RemoteHello {
+    @RequestMapping("/hello")
+    public String hello(@RequestParam(required = true, name = "name", defaultValue = "rxs1") String name);
+}
