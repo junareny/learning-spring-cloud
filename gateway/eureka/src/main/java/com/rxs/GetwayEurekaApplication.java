4cd4e26c8bd4a215695677e2cf1ca7cb17a4f9f1
renyajun <renyajun@szkingdom.com>
2020-12-08 11:16:25 +0800

    [提交人]任亚军 [时间]20201208 [内容]改名字：加上Getway

--- gateway/eureka/src/main/java/com/rxs/GetwayEurekaApplication.java
+++ gateway/eureka/src/main/java/com/rxs/GetwayEurekaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaApplication {
+public class GetwayEurekaApplication {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class, args);
+        SpringApplication.run(GetwayEurekaApplication.class, args);
     }
 }
--- gateway/producer/src/main/java/com/rxs/GetwayProducerApplication.java
+++ gateway/producer/src/main/java/com/rxs/GetwayProducerApplication.java
@@ -3,13 +3,11 @@ package com.rxs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
-public class ConsumerApplication {
+public class GetwayProducerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ConsumerApplication.class, args);
+        SpringApplication.run(GetwayProducerApplication.class, args);
     }
 }
